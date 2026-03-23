98d2c161b8cfc74354eb9f3ba73beccf5c9d3ea3
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-03-23 21:48:38 +0300

    storage for the calculation

--- calculator.js
+++ calculator.js
@@ -13,3 +13,26 @@ function calculate(num1, num2, operator) {
       return 'Invalid operator';
   }
 }
+
+// storage
+function savecalculation(num1, num2, operator, result) {
+    const calculation = {
+        num1: num1,
+        num2: num2,
+        operator: operator,
+        result: result
+    };
+    localStorage.setItem("calculation", JSON.stringify(calculation));
+
+}
+
+// history
+calculate.prototype.saveToStorage = function(num1, num2, operator, result) {
+    localStorage.setItem(`calculation`, JSON.stringify(calculation));
+
+}
+
+calculate.prototype.loadFromStorage = function(num1, num2, operator, result) {
+    const savedData = localStorage.getItem(`calculation`);
+    return savedData ? JSON.parse(savedData) : {};
+}
