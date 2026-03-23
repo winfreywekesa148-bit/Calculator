98d3231381d9833b76e5cb55314f0c9ed7176f81
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-03-23 21:22:20 +0300

    operators and the calculation

---
+++ calculator.js
@@ -0,0 +1,15 @@
+// calculation
+function calculate(num1, num2, operator) {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    case '/':
+      return num1 / num2;
+    default:
+      return 'Invalid operator';
+  }
+}
